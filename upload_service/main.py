5c5ec64c72af22650b908fef139b5ee800aff664
vishal <vishal23rfx@gmail.com>
2025-06-29 17:04:51 +0530

    Add upload router with multipart upload functionality and integrate S3 support

--- upload_service/main.py
+++ upload_service/main.py
@@ -8,6 +8,7 @@ import boto3
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from routers import upload
 from pydantic import BaseModel
 
 from kafka.kafka import KafkaProducer
@@ -27,11 +28,14 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_methods=["*"],
     allow_credentials=True,
     allow_headers=["*"]
 )
+
+app.include_router(upload.router, prefix="/upload", tags=["Upload"])
+
 
 @app.get("/")
 async def home():
@@ -39,42 +43,6 @@ async def home():
     Home endpoint that indicates the service is up.
     """
     return {"message": "Service is Up!"}
-
-
-@app.post('/upload')
-async def upload(file: UploadFile = File(...)):
-    """
-    Uploads a file to an S3 bucket using boto3.
-
-    This function retrieves the S3 bucket name and AWS credentials from environment variables.
-    It uploads the provided file object to the specified S3 bucket. If the S3 bucket name is not set
-    or if any exception occurs during the upload, an HTTPException is raised.
-
-    Args:
-        file (UploadFile): The file object to be uploaded, provided by FastAPI's File dependency.
-
-    Raises:
-        HTTPException: If the S3_BUCKET_NAME environment variable is not set.
-        HTTPException: If an error occurs during the S3 upload process.
-
-    Returns:
-        dict: A dictionary containing a success message with the file name and S3 bucket name.
-    """
-    s3_bucket = os.getenv('S3_BUCKET_NAME')
-    if not s3_bucket:
-        raise HTTPException(status_code=500, detail="S3_BUCKET_NAME environment variable is not set")
-
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY'),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-    )
-
-    try:
-        s3_client.upload_fileobj(file.file, s3_bucket, file.filename)
-        return {"message": f"File {file.filename} uploaded successfully to {s3_bucket}"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/produce")
---
+++ upload_service/routers/upload.py
@@ -0,0 +1,107 @@
+import logging
+import boto3
+import os
+from dotenv import load_dotenv
+
+from fastapi import APIRouter, Form, UploadFile, HTTPException, Request
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+load_dotenv(override=True)
+
+router = APIRouter()
+s3_bucket = os.getenv('S3_BUCKET_NAME')
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name="ap-south-1"
+)
+
+@router.post('/')
+async def upload(
+    filename: str = Form(...),
+    upload_id: str = Form(..., alias="uploadId"),
+    part_id: int = Form(..., alias="chunkIndex"),
+    part: UploadFile = Form(..., alias="chunk")
+):
+    if not s3_bucket:
+        raise HTTPException(status_code=500, detail="S3_BUCKET_NAME environment variable is not set")
+    logger.debug("Part id is %s",part_id)
+    try:
+        resp = s3.upload_part(
+            Bucket = s3_bucket,
+            Key = filename,
+            UploadId = upload_id,
+            PartNumber = part_id,
+            Body = part.file
+        )
+
+        return {"etag": resp["ETag"], "chunkIndex": part_id}
+
+    except s3.exceptions.NoSuchUpload:
+        raise HTTPException(status_code=404, detail="Upload ID not found")
+    except Exception as e:
+        logger.error("Exception while uploading part %s", str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post('/initialize')
+async def initialize(filename: str = Form(...)):
+    if not s3_bucket:
+        raise HTTPException(status_code=500, detail="S3_BUCKET_NAME environment variable is not set")
+
+    try:
+        resp = s3.create_multipart_upload(Bucket=s3_bucket, Key=filename)
+        upload_id = resp['UploadId']
+        return {
+            "message": "Multipart upload initialized successfully",
+            "uploadId": upload_id
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post('/complete')
+async def complete(request: Request):
+    """
+        Expects a JSON body like:
+        {
+            "filename": "file.mp4",
+            "uploadId": "xyz",
+            "parts": [
+                {"part_number": 1, "etag": "\"8e9b...\""},
+                {"part_number": 2, "etag": "\"9f01...\""}
+            ]
+        }
+    """
+    try:
+        data = await request.json()
+        print("DATA", data)
+        filename = data["filename"]
+        upload_id = data["uploadId"]
+        parts_list = data["parts"]
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid JSON body")
+
+    print("part list", parts_list)
+    for p in parts_list:
+        print(type(p), p)
+    parts_sorted = sorted(
+        [{"PartNumber": p["chunkIndex"], "ETag": p["etag"]} for p in parts_list],
+        key=lambda p: p["PartNumber"],
+    )
+
+    try:
+        s3.complete_multipart_upload(
+            Bucket=s3_bucket,
+            Key=filename,
+            UploadId=upload_id,
+            MultipartUpload={'Parts': parts_sorted}
+        )
+        return {"message": "File uploaded successfully"}
+    except Exception as e:
+        logger.error("Exception while completing upload: %s", str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post('/abort')
+async def abort_upload(upload_id: str, filename: str):
+    s3.abort_multipart_upload(Bucket = s3_bucket, Key=filename, UploadId=upload_id)
