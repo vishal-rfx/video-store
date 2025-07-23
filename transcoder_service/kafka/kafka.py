171ccc8265760bf7737bf77a0d6bdd8b0c8a80f0
vishal <vishal23rfx@gmail.com>
2025-07-23 08:38:17 +0530

    update kafka and db configuration

--- transcoder_service/kafka/kafka.py
+++ transcoder_service/kafka/kafka.py
@@ -1,7 +1,6 @@
 import json
 from dotenv import load_dotenv
 import os
-import ssl
 import asyncio
 import traceback
 
@@ -12,23 +11,15 @@ load_dotenv(override=True)
 
 class KafkaConfig:
     def __init__(self, consumer_group_id: str):
-        self.bootstrap_servers = os.getenv("KAFKA_SERVER_URI")
-        self.security_protocol = "SASL_SSL"
-        self.sasl_mechanism = "SCRAM-SHA-256"
-        self.sasl_plain_username = os.getenv("KAFKA_USER")
-        self.sasl_plain_password = os.getenv("KAFKA_PASSWORD")
+        self.bootstrap_servers = os.getenv("KAFKA_SERVER_URI", "kafka:29092")
+        self.security_protocol = "PLAINTEXT"
         self.group_id = consumer_group_id
-        self.ssl_ctx = ssl.create_default_context(cafile=os.getenv("KAFKA_CA_FILE"))
 
     def to_dict(self):
         return {
             "bootstrap_servers": self.bootstrap_servers,
             "security_protocol": self.security_protocol,
-            "sasl_mechanism": self.sasl_mechanism,
-            "sasl_plain_username": self.sasl_plain_username,
-            "sasl_plain_password": self.sasl_plain_password,
-            "ssl_context": self.ssl_ctx,
-            "group_id" : self.group_id
+            "group_id": self.group_id
         }
 
 class KafkaConsumer:
--- upload_service/db/database.py
+++ upload_service/db/database.py
@@ -1,13 +1,10 @@
-import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from core.config import settings
 
-ssl_context = ssl.create_default_context(cafile=settings.PG_CA_FILE)
 engine = create_async_engine(
     str(settings.PG_DATABASE_URL),
     echo=False,
-    connect_args={"ssl": ssl_context}
 )
 
 SessionLocal = async_sessionmaker(
