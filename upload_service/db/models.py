78344713f107b2bafac8317d101566c26ecaaf4a
vishal <vishal23rfx@gmail.com>
2025-07-23 08:07:23 +0530

    - add local kafka and postgres
    - add new HLS model to the database schema for managing HLS streams associated with video metadata.

--- upload_service/db/models.py
+++ upload_service/db/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, DateTime, func
+from sqlalchemy import Integer, String, Text, DateTime, func, ForeignKey
 from db.database import Base
 
 class VideoMetaData(Base):
@@ -12,4 +12,13 @@ class VideoMetaData(Base):
     description: Mapped[str | None] = mapped_column(Text, nullable=True)
     author: Mapped[str | None] = mapped_column(String(100), nullable=True)
     key: Mapped[str] = mapped_column(Text, unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
+class HLS(Base):
+    __tablename__ = "hls"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    video_metadata_id: Mapped[int] = mapped_column(Integer, ForeignKey("video_metadata.id"), nullable=False)
+    hls_key: Mapped[str] = mapped_column(Text, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
